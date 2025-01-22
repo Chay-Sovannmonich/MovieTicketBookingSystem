684f726895a3143bf3436c261acf43fd8a99814c
Chay-Sovannmonich <sovannmonich.chay@student.cadt.edu.kh>
2025-01-22 12:11:37 +0700

    Add files via upload

---
+++ Project/MovieBooking.java
@@ -0,0 +1,12 @@
+package Project;
+
+class User {
+
+    String name, username, address, password, phone_number;
+    
+}
+public class MovieBooking {
+    public static void main(String[] args) {
+        
+    }
+}
